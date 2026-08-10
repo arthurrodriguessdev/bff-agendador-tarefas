b830041ea6d68191bcaf0ed42d3927dc01b08653
Arthur <arthurdev.contatoo@gmail.com>
2026-08-10 15:25:11 -0300

    adicionando cors na aplicacao

---
+++ src/main/java/agendador/bffagendadortarefas/infraestructure/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package agendador.bffagendadortarefas.infraestructure.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer configCors(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://localhost:4200")
+                        .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(360);
+            }
+        };
+    }
+}
